b46a15003b3b652711aded3c5cd4444dfa2fc9ef
soon9 <81350489+hsk980911@users.noreply.github.com>
2022-11-30 21:35:42 +0900

    221130

---
+++ 황순규/코딩테스트/구현/2167_bj.py
@@ -0,0 +1,17 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+arr = []
+
+for _ in range(N):
+    arr.append(list(map(int, sys.stdin.readline().split())))
+    
+dp = [[0]*(M+1) for _ in range(N+1)]
+for i in range(1, N+1):
+    for j in range(1, M+1):
+        dp[i][j] =arr[i-1][j-1] - dp[i-1][j-1] + dp[i][j-1] + dp[i-1][j]
+
+k = int(sys.stdin.readline())
+for _ in range(k):
+    i, j, x, y = map(int, sys.stdin.readline().split())
+    print(dp[x][y] - dp[i-1][y] - dp[x][j-1] + dp[i-1][j-1])
